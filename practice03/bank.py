b67c6d68e870c7ae5aa673223c0bf3186c68cbf1
Kirill <smakvlad@gmail.com>
2020-10-22 17:52:44 +0300

    dz

--- practice03/bank.py
+++ practice03/bank.py
@@ -1,13 +1,8 @@
 suma = int(input())
-a = 0 
+
 bank = [1,2,5,10,20,50,100,200,500,1000] 
 for i in bank:     
-	while a <= 10:
-		quantity = suma // i 
-		suma %= i 
-		a += 1 
-		if quantity == 0: 
-			continue
-		print(quantity, i) 
-
+	quantity = suma // i 
+	suma %= i	 
+	print(str(quantity) +" : "+ str(i)) 
 
