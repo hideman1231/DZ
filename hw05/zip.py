f170c78881cfc8020ae89aa972aee1e3aaffb7b2
Kirill <smakvlad@gmail.com>
2020-11-04 14:18:32 +0200

    zip

--- hw05/zip.py
+++ hw05/zip.py
@@ -1,17 +1,14 @@
 # Разбираемся, что делает функция zip, и пробуем написать свой собственный zip 
 
-students = dict(Oleg=[10,8,7,10],Dima=[11,4,5,8],Gleb=[10,2,12,12,6],Ivan=[7,1,9])
+a = ['Denis', 'Doc', 'Kirill','Dima'] 
+b = [12, 17] 
 
-a = []
+while len(a) > len(b): 
+	del a[-1] 
 
-for value in students.values():
-	b = sum(value) // len(value)
-	a.append(b)
-print(list(zip(students, a))) 
+for i in range(len(a)): 
+	print(a[i],b[i]) 
 
-a = [1,2,3] 
-b = [5,10,15] 
-print(list(zip(a,b)))
 
 
 
