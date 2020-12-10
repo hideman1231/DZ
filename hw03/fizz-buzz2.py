be7fd0eba17abc9cfad91fb884880f9f907431ca
Kirill <smakvlad@gmail.com>
2020-12-10 15:30:36 +0200

    up

--- hw03/fizz-buzz2.py
+++ hw03/fizz-buzz2.py
@@ -3,18 +3,15 @@
 f = open("ff.txt", "r", encoding="UTF-8") 
 for line in f:
 	line = line.split() 
-	fizz = line[0]
-	buzz = line[1]
-	gezz = line[2]
 	f = open("fizzbuzz.txt", "a")
-	for a in range(1,int(gezz)+1): 
-		if (a % int(fizz)) == 0 and (a % int(buzz)) == 0:
-			f.write("FB")  
-		elif a % int(buzz) == 0: 
-			f.write("B") 
-		elif a % int(fizz) == 0:
-			f.write("F")  
+	for a in range(1,int(line[2])+1): 
+		if (a % int(line[0])) == 0 and (a % int(line[1])) == 0:
+			f.write("FB ")  
+		elif a % int(line[1]) == 0: 
+			f.write("B ") 
+		elif a % int(line[0]) == 0:
+			f.write("F ")  
 		else: 
-			f.write(str(a)) 
+			f.write(f"{a} ") 
 	
 f.close() 
